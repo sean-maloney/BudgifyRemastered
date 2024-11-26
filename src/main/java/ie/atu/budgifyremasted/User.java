c9bd4191492ffe5bae83959553245bcf8b7218a4
G00419446@atu.ie <jseeruthen26@outlook.com>
2024-11-26 12:59:16 +0000

    Started on incorparating MySQL database and Tyrese added start of login in service

--- src/main/java/ie/atu/budgifyremasted/User.java
+++ src/main/java/ie/atu/budgifyremasted/User.java
@@ -1,9 +1,6 @@
 package ie.atu.budgifyremasted;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,16 +10,24 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name= "users")
 public class User {
 
     @Id
     @NotNull(message = "User Id cant be blank")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long userId;
+    @Column(name= "users_Id")
+    private Long user_Id;
+
+    @NotBlank(message = "name cannot be blank")
+    @Column(name= "name")
+    private String name;
 
     @NotBlank(message = "Username cannot be blank")
+    @Column(name= "username")
     private String username;
-
+    
+    @Column(name= "email")
     @Email(message = "email cant be blank")
     private String email;
 
@@ -30,15 +35,26 @@ public class User {
     private int age;
 
     @Size(max = 24, min = 8, message = "Password cannot be blank, more than 24 characters and less than 8 characters")
+    @Column(name= "password")
     private String password;
 
+    @Column(name= "country")
     @NotBlank(message = "Country cannot be blank")
     private String country;
 
+    @Column(name= "gender")
     //@Pattern(regexp = "male" + "female", message = "Gender cannot be blank")
     private String gender;
 
+    @Column(name= "subscriptionId")
     //@Pattern(regexp = "Student" + "Family" + "Free" + "Solo" + "Duo", message = "Gender cannot be blank")
     private String subscriptionId;
 
+    @Column(name= "Date_of_birth")
+    //@Pattern(regexp = "Student" + "Family" + "Free" + "Solo" + "Duo", message = "Gender cannot be blank")
+    private String Date_of_birth;
+
+    @Column(name= "Parental_controls")
+    @
+    private String Parental_controls;
 }
